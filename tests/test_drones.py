bd55367eaa7dedc7b4889b4f590f9656af4be5be
Leynier Gutiérrez González <leynier41@gmail.com>
2022-05-28 04:01:39 -0400

    tests: fix errors in tests

--- tests/test_drones.py
+++ tests/test_drones.py
@@ -5,10 +5,14 @@ from uuid import UUID, uuid4
 import pytest
 from dirty_equals import HasLen, IsList, IsPartialDict, IsUUID
 from drones.main import app
+from drones.settings import Settings, get_settings
 from fastapi.testclient import TestClient
 from mimesis.providers import Internet, Numeric, Person
 
-from .mocks import get_drone_mock_repository, get_medication_mock_repository
+
+@pytest.fixture(name="settings")
+def fixture_settings() -> Settings:
+    return get_settings()
 
 
 @pytest.fixture(name="faker_internet")
@@ -46,17 +50,20 @@ def test_index_redirect_to_docs(client: TestClient) -> None:
 def test_post_drone(
     client: TestClient,
     faker_numeric: Numeric,
+    settings: Settings,
 ) -> None:
     global drone, drone_id
     drone = {
         "serial_number": str(faker_numeric.integer_number(start=0, end=10**6)),
         "model": faker_numeric.integer_number(start=0, end=3),
         "weight_limit": faker_numeric.integer_number(start=1, end=400),
-        "battery_capacity": faker_numeric.integer_number(start=0, end=100),
+        "battery_capacity": faker_numeric.integer_number(
+            start=settings.min_battery_capacity_for_loading + 1, end=100
+        ),
         "state": faker_numeric.integer_number(start=0, end=5),
     }
     response = client.post("/drones", json=drone)
-    response.raise_for_status()
+    assert response.status_code == 200, response.text
     data = response.json()
     assert data == IsPartialDict(**drone)
     assert data == IsPartialDict(id=IsUUID)
@@ -66,7 +73,7 @@ def test_post_drone(
 @pytest.mark.depends(on=[test_post_drone.__name__])
 def test_get_drone(client: TestClient) -> None:
     response = client.get(f"/drones/{drone_id}")
-    response.raise_for_status()
+    assert response.status_code == 200, response.text
     data = response.json()
     assert data == IsPartialDict(**drone)
     assert data == IsPartialDict(id=str(drone_id))
@@ -75,7 +82,7 @@ def test_get_drone(client: TestClient) -> None:
 @pytest.mark.depends(on=[test_get_drone.__name__])
 def test_get_drones(client: TestClient) -> None:
     response = client.get("/drones", json=drone)
-    response.raise_for_status()
+    assert response.status_code == 200, response.text
     data = response.json()
     assert data == IsList(IsPartialDict(**drone, id=str(drone_id)))
 
@@ -83,7 +90,7 @@ def test_get_drones(client: TestClient) -> None:
 @pytest.mark.depends(on=[test_get_drones.__name__])
 def test_get_drones_by_state(client: TestClient) -> None:
     response = client.get("/drones", params={"state": drone["state"]})
-    response.raise_for_status()
+    assert response.status_code == 200, response.text
     data = response.json()
     assert data == IsList(IsPartialDict(**drone, id=str(drone_id)))
 
@@ -98,12 +105,12 @@ def test_post_medication(
     global medication, medication_id
     medication = {
         "name": faker_person.name(),
-        "weight": faker_numeric.integer_number(start=1, end=drone["weight_limit"]),
+        "weight": faker_numeric.integer_number(start=1, end=drone["weight_limit"] - 1),
         "code": str(faker_numeric.integer_number(start=0)),
         "image": faker_internet.url(),
     }
     response = client.post(f"/drones/{drone_id}/medications", json=medication)
-    response.raise_for_status()
+    assert response.status_code == 200, response.text
     data = response.json()
     assert data == IsPartialDict(**medication)
     assert data == IsPartialDict(id=IsUUID, drone_id=str(drone_id))
@@ -139,7 +146,7 @@ def test_delete_drone_with_medications(client: TestClient) -> None:
 @pytest.mark.depends(on=[test_delete_drone_with_medications.__name__])
 def test_get_medication(client: TestClient) -> None:
     response = client.get(f"/drones/{drone_id}/medications")
-    response.raise_for_status()
+    assert response.status_code == 200, response.text
     data = response.json()
     assert data == IsList(IsPartialDict(**medication, id=str(medication_id)))
 
@@ -147,13 +154,13 @@ def test_get_medication(client: TestClient) -> None:
 @pytest.mark.depends(on=[test_get_medication.__name__])
 def test_delete_medication(client: TestClient) -> None:
     response = client.delete(f"/drones/{drone_id}/medications/{medication_id}")
-    response.raise_for_status()
+    assert response.status_code == 200, response.text
 
 
 @pytest.mark.depends(on=[test_delete_medication.__name__])
 def test_get_medication_not_found(client: TestClient) -> None:
     response = client.get(f"/drones/{drone_id}/medications")
-    response.raise_for_status()
+    assert response.status_code == 200, response.text
     data = response.json()
     assert data == IsList()
     assert data == HasLen(0)
@@ -162,7 +169,7 @@ def test_get_medication_not_found(client: TestClient) -> None:
 @pytest.mark.depends(on=[test_get_medication_not_found.__name__])
 def test_delete_drone(client: TestClient) -> None:
     response = client.delete(f"/drones/{drone_id}")
-    response.raise_for_status()
+    assert response.status_code == 200, response.text
 
 
 @pytest.mark.depends(on=[test_delete_drone.__name__])
@@ -186,7 +193,7 @@ def test_post_medication_not_found_in_post(
 ) -> None:
     medication = {
         "name": faker_person.name(),
-        "weight": faker_numeric.integer_number(start=1, end=drone["weight_limit"]),
+        "weight": faker_numeric.integer_number(start=1, end=drone["weight_limit"] - 1),
         "code": str(faker_numeric.integer_number(start=0)),
         "image": faker_internet.url(),
     }
@@ -200,31 +207,35 @@ def test_delete_medication_not_found_in_delete(client: TestClient) -> None:
     assert response.status_code == HTTPStatus.NOT_FOUND
 
 
-def test_insert_drone_with_20_of_batery(
+def test_insert_drone_with_low_of_batery(
     client: TestClient,
     faker_internet: Internet,
     faker_numeric: Numeric,
     faker_person: Person,
+    settings: Settings,
 ) -> None:
     drone = {
         "serial_number": str(faker_numeric.integer_number(start=0, end=10**6)),
         "model": faker_numeric.integer_number(start=0, end=3),
         "weight_limit": faker_numeric.integer_number(start=1, end=400),
-        "battery_capacity": faker_numeric.integer_number(start=0, end=20),
+        "battery_capacity": faker_numeric.integer_number(
+            start=0,
+            end=settings.min_battery_capacity_for_loading - 1,
+        ),
         "state": faker_numeric.integer_number(start=0, end=5),
     }
     response = client.post("/drones", json=drone)
-    response.raise_for_status()
+    assert response.status_code == 200, response.text
     data = response.json()
     assert data == IsPartialDict(**drone, id=IsUUID)
     drone_id = UUID(data["id"])
     medication = {
         "name": faker_person.name(),
-        "weight": faker_numeric.integer_number(start=1, end=drone["weight_limit"]),
+        "weight": faker_numeric.integer_number(start=1, end=drone["weight_limit"] - 1),
         "code": str(faker_numeric.integer_number(start=0)),
         "image": faker_internet.url(),
     }
     response = client.post(f"/drones/{drone_id}/medications", json=medication)
     assert response.status_code == HTTPStatus.BAD_REQUEST
     response = client.delete(f"/drones/{drone_id}")
-    response.raise_for_status()
+    assert response.status_code == 200, response.text
